5a0426e24adbcc6d15cf79dd906fe9826e24c68a
flp2113 <felipe.maia2113@gmail.com>
2026-02-24 11:55:12 -0300

    display_destroy_resources

--- src/display/display.c
+++ src/display/display.c
@@ -1,6 +1,20 @@
-#include "display.h"
-#include "../config.h"
 #include <SDL3/SDL.h>
+#include "../config.h"
+#include "display.h"
+
+static void display_destroy_resources(Display *display) {
+    if (display->texture)
+        SDL_DestroyTexture(display->texture);
+
+    if (display->renderer)
+        SDL_DestroyRenderer(display->renderer);
+
+    if (display->window)
+        SDL_DestroyWindow(display->window);
+
+    SDL_QuitSubSystem(display->init_flags);
+    *display = (Display){0};
+}
 
 bool display_initialize(Display *display, const DisplayConfig *config) {
     if (!display || !config)
@@ -13,36 +27,34 @@ bool display_initialize(Display *display, const DisplayConfig *config) {
     SDL_InitFlags init_flags = config->init_flags;
     SDL_RendererLogicalPresentation presentation = config->presentation;
 
-    display->window = NULL;
-    display->renderer = NULL;
-    display->texture = NULL;
+    *display = (Display){0};
     display->init_flags = init_flags;
 
     if (!SDL_InitSubSystem(init_flags))
-        goto cleanup;
+        goto failed;
 
     if (!SDL_CreateWindowAndRenderer(title, width, height, window_flags, &display->window, &display->renderer))
-        goto cleanup;
+        goto failed;
 
     display->texture = SDL_CreateTexture(display->renderer, SDL_PIXELFORMAT_RGBA32,
                                          SDL_TEXTUREACCESS_STREAMING, GRID_WIDTH, GRID_HEIGHT);
 
     if (!display->texture)
-        goto cleanup;
+        goto failed;
 
     if (!SDL_SetTextureScaleMode(display->texture, SDL_SCALEMODE_NEAREST))
-        goto cleanup;
+        goto failed;
 
     if (!SDL_SetRenderLogicalPresentation(display->renderer, width, height, presentation))
-        goto cleanup;
+        goto failed;
 
     if (!SDL_SetRenderVSync(display->renderer, 1))
-        goto cleanup;
+        goto failed;
 
     return true;
 
-cleanup:
-    display_cleanup(display);
+failed:
+    display_destroy_resources(display);
     return false;
 }
 
@@ -50,15 +62,5 @@ void display_cleanup(Display *display) {
     if (!display)
         return;
 
-    if (display->texture)
-        SDL_DestroyTexture(display->texture);
-
-    if (display->renderer)
-        SDL_DestroyRenderer(display->renderer);
-
-    if (display->window)
-        SDL_DestroyWindow(display->window);
-
-    SDL_QuitSubSystem(display->init_flags);
-    *display = (Display){0};
+    display_destroy_resources(display);
 }
