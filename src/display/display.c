fab02805bbeedb95321db09f117bbf4b73032a61
flp2113 <felipe.maia2113@gmail.com>
2026-02-24 10:34:55 -0300

    dry display_cleanup

--- src/display/display.c
+++ src/display/display.c
@@ -15,48 +15,46 @@ bool display_initialize(Display *display, const DisplayConfig *config) {
 
     display->window = NULL;
     display->renderer = NULL;
+    display->texture = NULL;
     display->init_flags = init_flags;
 
     if (!SDL_InitSubSystem(init_flags))
-        return false;
+        goto cleanup;
 
     if (!SDL_CreateWindowAndRenderer(title, width, height, window_flags, &display->window, &display->renderer))
-        goto quit_subsystem;
+        goto cleanup;
 
     display->texture = SDL_CreateTexture(display->renderer, SDL_PIXELFORMAT_RGBA32,
                                          SDL_TEXTUREACCESS_STREAMING, GRID_WIDTH, GRID_HEIGHT);
 
     if (!display->texture)
-        goto destroy_all;
+        goto cleanup;
 
     if (!SDL_SetTextureScaleMode(display->texture, SDL_SCALEMODE_NEAREST))
-        goto destroy_all;
+        goto cleanup;
 
     if (!SDL_SetRenderLogicalPresentation(display->renderer, width, height,
                                           presentation))
-        goto destroy_all;
+        goto cleanup;
 
     if (!SDL_SetRenderVSync(display->renderer, 1))
-        goto destroy_all;
+        goto cleanup;
 
     return true;
 
-destroy_all:
-    SDL_DestroyRenderer(display->renderer);
-    SDL_DestroyWindow(display->window);
-    SDL_DestroyTexture(display->texture);
-    SDL_QuitSubSystem(init_flags);
-    display->renderer = NULL;
-    display->window = NULL;
-    display->texture = NULL;
-quit_subsystem:
-    SDL_QuitSubSystem(init_flags);
+cleanup:
+    display_cleanup(display);
     return false;
 }
 
 void display_cleanup(Display *display) {
     if (!display)
         return;
+
+    if (display->texture) {
+        SDL_DestroyTexture(display->texture);
+        display->texture = NULL;
+    }
 
     if (display->renderer) {
         SDL_DestroyRenderer(display->renderer);
@@ -66,11 +64,6 @@ void display_cleanup(Display *display) {
     if (display->window) {
         SDL_DestroyWindow(display->window);
         display->window = NULL;
-    }
-
-    if (display->texture) {
-        SDL_DestroyTexture(display->texture);
-        display->texture = NULL;
     }
 
     SDL_QuitSubSystem(display->init_flags);
