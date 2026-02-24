af246ea21423e6a38d0b0a245461c4765bd2b758
flp2113 <felipe.maia2113@gmail.com>
2026-02-24 09:41:27 -0300

    removing unnecessary logging

--- src/display/display.c
+++ src/display/display.c
@@ -1,17 +1,10 @@
-#include <SDL3/SDL.h>
 #include "display.h"
 #include "../config.h"
+#include <SDL3/SDL.h>
 
 bool display_initialize(Display *display, const DisplayConfig *config) {
-    if (!display) {
-        SDL_Log("Couldn't find Display at Display initialization.");
+    if (!display || !config)
         return false;
-    }
-
-    if (!config) {
-        SDL_Log("Couldn't find DisplayConfig at Display initialization.");
-        return false;
-    }
 
     const char *title = config->title;
     int width = config->width;
@@ -24,44 +17,27 @@ bool display_initialize(Display *display, const DisplayConfig *config) {
     display->renderer = NULL;
     display->init_flags = init_flags;
 
-    if (!SDL_InitSubSystem(init_flags)) {
-        SDL_Log("Couldn't initialize SDL subsystem: %s", SDL_GetError());
+    if (!SDL_InitSubSystem(init_flags))
         return false;
-    }
 
-    if (!SDL_CreateWindowAndRenderer(title, width, height, window_flags,
-                                     &display->window, &display->renderer)) {
-        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+    if (!SDL_CreateWindowAndRenderer(title, width, height, window_flags, &display->window, &display->renderer))
         goto quit_subsystem;
-    }
 
-    display->texture = SDL_CreateTexture(
-        display->renderer,
-        SDL_PIXELFORMAT_RGBA32,
-        SDL_TEXTUREACCESS_STREAMING,
-        GRID_WIDTH, GRID_HEIGHT
-    );
+    display->texture = SDL_CreateTexture(display->renderer, SDL_PIXELFORMAT_RGBA32,
+                                         SDL_TEXTUREACCESS_STREAMING, GRID_WIDTH, GRID_HEIGHT);
 
-    if (!display->texture) {
-        SDL_Log("Couldn't create texture: %s", SDL_GetError());
+    if (!display->texture)
         goto destroy_all;
-    }
 
-    if (!SDL_SetTextureScaleMode(display->texture, SDL_SCALEMODE_NEAREST)) {
-        SDL_Log("Couldn't set texture scale mode: %s", SDL_GetError());
+    if (!SDL_SetTextureScaleMode(display->texture, SDL_SCALEMODE_NEAREST))
         goto destroy_all;
-    }
 
     if (!SDL_SetRenderLogicalPresentation(display->renderer, width, height,
-                                          presentation)) {
-        SDL_Log("Couldn't set logical presentation: %s", SDL_GetError());
+                                          presentation))
         goto destroy_all;
-    }
 
-    if (!SDL_SetRenderVSync(display->renderer, 1)) {
-        SDL_Log("Couldn't set Vsync: %s", SDL_GetError());
+    if (!SDL_SetRenderVSync(display->renderer, 1))
         goto destroy_all;
-    }
 
     return true;
 
@@ -79,10 +55,8 @@ quit_subsystem:
 }
 
 void display_cleanup(Display *display) {
-    if (!display) {
-        SDL_Log("Couldn't find Display at cleanup.");
+    if (!display)
         return;
-    }
 
     if (display->renderer) {
         SDL_DestroyRenderer(display->renderer);
