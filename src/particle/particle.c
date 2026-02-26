0f677ea72cb8a92d96bc0439e983e6e410206206
flp2113 <felipe.maia2113@gmail.com>
2026-02-26 12:13:07 -0300

    fixed sand logic

--- src/particle/particle.c
+++ src/particle/particle.c
@@ -2,8 +2,76 @@
 #include "../config.h"
 #include "../grid/grid.h"
 
+static void particle_swap_in_grid(Grid *grid, Coordinates source, Coordinates destination) {
+    Particle temporary_particle = grid->particles[source.y][source.x];
+    grid->particles[source.y][source.x] = grid->particles[destination.y][destination.x];
+    grid->particles[destination.y][destination.x] = temporary_particle;
+}
+
+static bool is_particle_solid_at(Grid *grid, Coordinates coordinates) {
+    if (!grid || !grid_is_in_bounds(coordinates))
+        return true;
+    return grid->particles[coordinates.y][coordinates.x].type == ROCK;
+}
+
+static void particle_update_sand(Grid *grid, Coordinates coordinates) {
+    if (coordinates.y + 1 >= GRID_HEIGHT)
+        return;
+
+    Coordinates below = {coordinates.x, coordinates.y + 1};
+    Coordinates left = {coordinates.x - 1, coordinates.y};
+    Coordinates right = {coordinates.x + 1, coordinates.y};
+    Coordinates below_left = {coordinates.x - 1, coordinates.y + 1};
+    Coordinates below_right = {coordinates.x + 1, coordinates.y + 1};
+
+    bool is_below_empty = grid_is_particle_empty(grid, below);
+    bool is_below_left_empty = grid_is_particle_empty(grid, below_left);
+    bool is_below_right_empty = grid_is_particle_empty(grid, below_right);
+    bool can_go_below_left = is_below_left_empty && !is_particle_solid_at(grid, left);
+    bool can_go_below_right = is_below_right_empty && !is_particle_solid_at(grid, right);
+
+    if (is_below_empty) {
+        particle_swap_in_grid(grid, coordinates, below);
+        return;
+    }
+
+    if (!can_go_below_left && !can_go_below_right) {
+        return;
+    }
+
+    if (can_go_below_left && can_go_below_right) {
+        bool go_left = SDL_rand(2);
+        particle_swap_in_grid(grid, coordinates, go_left ? below_left : below_right);
+        return;
+    }
+
+    if (can_go_below_left) {
+        particle_swap_in_grid(grid, coordinates, below_left);
+        return;
+    }
+
+    if (can_go_below_right) {
+        particle_swap_in_grid(grid, coordinates, below_right);
+        return;
+    }
+}
+
+void particle_update_in_grid(Grid *grid, Coordinates coordinates) {
+    if (!grid || !grid_is_in_bounds(coordinates))
+        return;
+
+    Particle *particle = &grid->particles[coordinates.y][coordinates.x];
+    switch (particle->type) {
+    case SAND:
+        particle_update_sand(grid, coordinates);
+        break;
+    default:
+        break;
+    }
+}
+
 static Uint8 clamp_color_component(int value) {
-    if (value < 0) 
+    if (value < 0)
         return 0;
 
     if (value > 255)
@@ -54,65 +122,9 @@ SDL_Color particle_get_random_color_by_type(ParticleType type) {
     }
 }
 
-static void particle_swap_in_grid(Grid *grid, Coordinates source, Coordinates destination) {
-    Particle temporary_particle = grid->particles[source.y][source.x];
-    grid->particles[source.y][source.x] = grid->particles[destination.y][destination.x];
-    grid->particles[destination.y][destination.x] = temporary_particle;
-}
-
-static void particle_update_sand(Grid *grid, Coordinates coordinates) {
-    if (coordinates.y + 1 >= GRID_HEIGHT) 
-        return;
-
-    Coordinates below = {coordinates.x, coordinates.y + 1};
-    Coordinates below_left = {coordinates.x - 1, coordinates.y + 1};
-    Coordinates below_right = {coordinates.x + 1, coordinates.y + 1};
-
-    bool is_below_empty = grid_is_particle_empty(grid, below);
-    bool is_below_left_empty = grid_is_particle_empty(grid, below_left);
-    bool is_below_right_empty = grid_is_particle_empty(grid, below_right);
-
-    if (is_below_empty) {
-        particle_swap_in_grid(grid, coordinates, below);
-        return;
-    }
-
-    if (!is_below_left_empty && !is_below_right_empty) {
-        return;
-    }
-
-    if (is_below_left_empty && is_below_right_empty) {
-        bool go_left = SDL_rand(2);
-        particle_swap_in_grid(grid, coordinates,
-                             go_left ? below_left : below_right);
-        return;
-    }
-
-    if (is_below_left_empty && !is_below_right_empty) {
-        particle_swap_in_grid(grid, coordinates, below_left);
-        return;
-    }
-
-    if (!is_below_left_empty && is_below_right_empty) {
-        particle_swap_in_grid(grid, coordinates, below_right);
-        return;
-    }
-}
-
-void particle_update_in_grid(Grid *grid, Coordinates coordinates) {
-    if (!grid || !grid_is_in_bounds(coordinates)) 
-        return;
-
-    Particle *particle = &grid->particles[coordinates.y][coordinates.x];
-    switch (particle->type) {
-        case SAND: particle_update_sand(grid, coordinates); break;
-        default: break;
-    }
-}
-
 bool particle_is_empty(const Particle *particle) {
-    if (!particle) 
+    if (!particle)
         return false;
-        
+
     return particle->type == EMPTY;
 }
--- src/particle/particle.h
+++ src/particle/particle.h
@@ -3,6 +3,7 @@
 
 #include <SDL3/SDL.h>
 #include <stdbool.h>
+
 #include "../types.h"
 
 typedef struct grid Grid;
@@ -15,13 +16,13 @@ typedef struct particle {
     SDL_Color color;
 } Particle;
 
+void particle_update_in_grid(Grid *grid, Coordinates coordinates);
+
 SDL_Color particle_get_random_sand_color(void);
 SDL_Color particle_get_random_rock_color(void);
 SDL_Color particle_get_default_color_by_type(ParticleType type);
 SDL_Color particle_get_random_color_by_type(ParticleType type);
 
 bool particle_is_empty(const Particle *particle);
-
-void particle_update_in_grid(Grid *grid, Coordinates coordinates);
 
 #endif
