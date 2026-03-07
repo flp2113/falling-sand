19da860aa1825adcb1cf2565500d7649dc3cecfc
flp2113 <felipe.maia2113@gmail.com>
2026-03-07 13:47:08 -0300

    fixed timestep simulation

--- src/main.c
+++ src/main.c
@@ -3,7 +3,8 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <time.h>
-#include "config.h"
+
+#include "config/display_config.h"
 #include "display/display.h"
 #include "grid/grid.h"
 
@@ -13,6 +14,8 @@ typedef struct app_state {
     bool left_mouse_pressed;
     ParticleType particle_in_use;
     int brush_radius;
+    Uint64 last_tick;
+    double accumulator;
 } AppState;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
@@ -39,13 +42,15 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
 
     if (!grid_initialize(&state->grid)) {
         SDL_Log("Couldn't initialize Grid.");
-        display_cleanup(&state->display);
+        display_destroy(&state->display);
         SDL_free(state);
         return SDL_APP_FAILURE;
     }
 
     state->brush_radius = DEFAULT_BRUSH_RADIUS;
     state->particle_in_use = SAND;
+    state->last_tick = SDL_GetPerformanceCounter();
+    state->accumulator = 0.0;
 
     SDL_srand((Uint64)time(NULL));
 
@@ -65,7 +70,7 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
             case SDLK_1: state->particle_in_use = SAND; break;
             case SDLK_2: state->particle_in_use = ROCK; break;
             case SDLK_3: state->particle_in_use = EMPTY; break;
-            case SDLK_R: grid_cleanup(&state->grid); break;
+            case SDLK_R: grid_reset(&state->grid); break;
             default: break;
         }
     } 
@@ -98,15 +103,23 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
     if (!state || !state->display.renderer) 
         return SDL_APP_FAILURE;
 
+    Uint64 now = SDL_GetPerformanceCounter();
+    double dt = (double)(now - state->last_tick) / (double)SDL_GetPerformanceFrequency();
+    state->last_tick = now;
+    state->accumulator += dt;
+
     float mouse_x, mouse_y;
     SDL_GetMouseState(&mouse_x, &mouse_y);
     Coordinates coordinates = {(int)(mouse_x / PARTICLE_SIZE), (int)(mouse_y / PARTICLE_SIZE)};
     if (state->left_mouse_pressed && grid_is_in_bounds(coordinates)) 
         grid_apply_brush(&state->grid, coordinates, state->brush_radius, state->particle_in_use);
-    
-    grid_update(&state->grid);
+
+    while (state->accumulator >= SIMULATION_TICK_RATE) {
+        grid_update(&state->grid);
+        state->accumulator -= SIMULATION_TICK_RATE;
+    }
+
     grid_render(&state->grid, &state->display);
-    
     SDL_RenderPresent(state->display.renderer);
 
     return SDL_APP_CONTINUE;
@@ -115,8 +128,7 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
     AppState *state = appstate;
     if (state) {
-        grid_cleanup(&state->grid);
-        display_cleanup(&state->display);
+        display_destroy(&state->display);
         SDL_free(state);
     }
 }
